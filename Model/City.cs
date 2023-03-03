b7443de1d7f37c160fc725b3288c7753ac6ec045
raogpip <vitalik.fedorenko.2018@gmail.com>
2023-03-03 14:04:04 +0100

    Model and API connector

---
+++ Model/City.cs
@@ -0,0 +1,19 @@
+﻿namespace WeatherApp.WPF.Learning.Model
+{
+    public class City
+    {
+        public int Version { get; set; }
+        public string Key { get; set; }
+        public string Type { get; set; }
+        public int Rank { get; set; }
+        public string LocalizedName { get; set; }
+        public Area Country { get; set; }
+        public Area AdministrativeArea { get; set; }
+    }
+
+    public class Area
+    {
+        public string ID { get; set; }
+        public string LocalizedName { get; set; }
+    }
+}
---
+++ ViewModel/Helpers/AccuWeatherHelper.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WeatherApp.WPF.Learning.Model;
+
+namespace WeatherApp.WPF.Learning.ViewModel.Helpers
+{
+    public class AccuWeatherHelper
+    {
+        public const string BASE_URL = "http://dataservice.accuweather.com/";
+        public const string AUTOCOMPLETE_ENDPOINT = "locations/v1/cities/autocomplete?apikey={0}&q={1}";
+        public const string CURRENT_CONDITIONS_ENDPOINT = "currentconditions/v1/{0}?apikey={1}";
+        public const string API_KEY = "ZuUiGJLZNTa9XAXMjwTAuPvT4wMlNqNR";
+
+        public static async Task<List<City>> GetCities(string query)
+        {
+            var cities = new List<City>();
+
+            string url = BASE_URL + string.Format(AUTOCOMPLETE_ENDPOINT, API_KEY, query);
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+                cities = JsonConvert.DeserializeObject<List<City>>(json);
+            }
+
+            return cities;
+        }
+
+        public static async Task<CurrentConditions> GetCurrentConditions(string cityKey)
+        {
+            var conditions = new CurrentConditions();
+
+            string url = BASE_URL + string.Format(CURRENT_CONDITIONS_ENDPOINT, cityKey, API_KEY);
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+                conditions = JsonConvert.DeserializeObject<List<CurrentConditions>>(json).FirstOrDefault();
+            }
+
+            return conditions;
+        }
+    }
+}
+ 
