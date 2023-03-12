ff611033bd414d3c4760b8d7a9e0c349cce44730
raogpip <vitalik.fedorenko.2018@gmail.com>
2023-03-12 14:45:35 +0100

    IValueConverter bool to rain implementation. Final commit

---
+++ ViewModel/ValueConverters/BoolToRainConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WeatherApp.WPF.Learning.ViewModel.ValueConverters
+{
+    class BoolToRainConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isRaining = (bool)value;
+
+            if (isRaining)
+            {
+                return "Currently raining.";
+            }
+            else
+                return "Sky is clear.";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string isRaining = (string)value;
+
+            if (isRaining == "Currently raining.")
+                return true;
+
+            return false;
+        }
+    }
+}
